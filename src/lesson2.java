046b7d34f22c5a194db3ee4342188eaeb8f4b3f5
Sherzod <99177182+Sherzod0411@users.noreply.github.com>
2022-08-03 12:04:57 +0300

    Update lesson2.java

--- src/lesson2.java
+++ src/lesson2.java
@@ -4,7 +4,7 @@ public class lesson2 {
     public static void main(String[] args) {
         Scanner enter = new Scanner(System.in);
         System.out.print("Enter your name= ");
-        String user = enter.next();
+        String user = enter.nextLine();
         System.out.println("Hello "+user);
 
     }
