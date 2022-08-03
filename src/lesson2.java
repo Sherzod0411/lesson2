7b7a26e2236daa181351aceee7e361238eeec799
Sherzod <99177182+Sherzod0411@users.noreply.github.com>
2022-08-03 11:35:43 +0300

    Initial commit

---
+++ src/lesson2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class lesson2 {
+    public static void main(String[] args) {
+        Scanner enter = new Scanner(System.in);
+        System.out.print("Enter your name= ");
+        String user = enter.next();
+        System.out.println("Hello "+user);
+
+    }
+}
